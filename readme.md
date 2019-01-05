d225cd57f091183431dab45ccda360f6f4e4890c
Jonathan Schmold <me@jonathanschmold.ca>
2019-01-04 19:12:28 -0700

    Removed unnecessary files from repo

--- readme.md
+++ readme.md
@@ -2,3 +2,9 @@
 
 To create a basic platformer with jumping, some coins, and a single end-goal
 
+
+## Notes
+
+Need to create a basic generic type that encapsulates a data identification for the fixture userDef.
+
+Has the type and pointer.
--- src/entities/world.cpp
+++ src/entities/world.cpp
@@ -13,7 +13,7 @@ World::World(RenderTarget* renderer, b2Vec2 gravity) : b2World(gravity)
     this->m_renderer  = renderer;
 
     this->m_view      = new View(FloatRect(0, 0, 1.f, 1.f));
-    this->m_view->setSize(32.f, 18.f);
+    this->m_view->setSize(64.f, 36.f);
     this->m_view->setViewport(FloatRect(0.f, 0.f, 1.f, 1.f));
 
     renderer->setView(**this);
